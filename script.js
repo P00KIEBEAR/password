3a6e89cc07d352c333d317397dcd955faaabc5d8
pat paggi <patpaggi@gmail.com>
2020-10-08 10:34:03 -0700

    not working

--- script.js
+++ script.js
@@ -16,20 +16,13 @@ function writePassword() {
   parseInt(characterAmount);
   var generatePassword = " ";
   //Lowercase letter
-  var lower = window.confirm("Would you like to include Lowercase?");
-  const lowercaseCharacterSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  if (lower === true) {
-    var letter = (Math.floor(Math.random() * 25));
-    parseInt(letter);
-    var newLetter = (lowercaseCharacterSet[letter]);
-    generatePassword = generatePassword + newLetter;
-    (characterAmount = characterAmount - 1);
-  } else { }
+  //var lower = window.confirm("Would you like to include Lowercase?");
+
   //uppercase
   var upper = window.confirm("Would you like to include Uppercase?");
   var uppercaseCharacterSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   if (upper === true) {
-    var upLetter = (Math.floor(Math.random() * 25));
+    var upLetter = (Math.floor(Math.random() * uppercaseCharacterSet.length));
     parseInt(upLetter);
 
     var newUpLetter = (uppercaseCharacterSet[upLetter]);
@@ -38,21 +31,19 @@ function writePassword() {
   } else { }
   //Number
   var number = window.confirm("Would you like to include Number?");
-  var numberCharacterSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   if (number === true) {
-    var numbers = (Math.floor(Math.random() * 10));
-    parseInt(numbers);
-    var newNumber = (numberCharacterSet[numbers]);
-    generatePassword = generatePassword + newNumber;
+    var numberCharacterSet = (Math.floor(Math.random() * 10));
+    generatePassword = generatePassword + numberCharacterSet;
     (characterAmount = characterAmount - 1);
   } else { }
   //Symbol
   var symbol = window.confirm("Would you like to include Symbol?");
   var symbolCharacterSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "/", "_", "+", ";", ":", "=", "|", "?"];
   if (symbol === true) {
-    var symbols = (Math.floor(Math.random() * 18));
+    var symbols = (Math.floor(Math.random() * symbolCharacterSet.length));
     parseInt(symbols);
     var newSymbol = (symbolCharacterSet[symbols]);
+
     generatePassword = generatePassword + newSymbol;
     (characterAmount = characterAmount - 1);
   } else { }
@@ -62,14 +53,44 @@ function writePassword() {
 
 
   while (characterAmount > 0) {
-    placeValue = (Math.floor(Math.random() * 10));
-    generatePassword = generatePassword + placeValue;
+    const lowercaseCharacterSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    //if (lower === true) {
+    var letter = (Math.floor(Math.random() * uppercaseCharacterSet.length));
+    parseInt(letter);
+    var newLetter = (lowercaseCharacterSet[letter]);
+    generatePassword = generatePassword + newLetter;
     (characterAmount = characterAmount - 1);
+    //}// else { }
   }
-  var password = generatePassword;
-  var passwordText = document.querySelector("#password");
+  shuffle()
+  /* NOTE: Got this shuffle algorithm from stackoverflow
+   https://stackoverflow.com/a/6274381
+   function shuffle(a) {
+   var j, x, i;
+   for (i = a.length - 1; i > 0; i--) {
+     j = Math.floor(Math.random() * (i + 1));
+     x = a[i];
+     a[i] = a[j];
+     a[j] = x;
+   }
+    return a;
+*/}
+function shuffle(generatePassword) {
+  var j, x, i;
+  for (i = generatePassword.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = generatePassword[i];
+    generatePassword[i] = generatePassword[j];
+    generatePassword[j] = x;
+  }
+  return generatePassword;
+}
 
-  passwordText.value = password;
+
+var password = generatePassword;
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
 }
 
 // Add event listener to generate button "when button is pushed"
